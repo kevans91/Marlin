23aefa7885c1bd2bc329bd90f6a2b85835d962bb
Kyle Evans <kevans91@ksu.edu>
2016-10-24 18:31:52 -0500

    Update script a little bit to use arduino-builder.options and place the output binary at ./flash.hex so that I may ./compile.sh && ./flash.sh when I need to update

--- compile.sh
+++ compile.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-arduino-builder -verbose -fqbn arduino:avr:mega:cpu=atmega2560 -hardware /usr/local/arduino/hardware  -tools /usr/local/arduino/tools-builder -core-api-version 10612 -libraries /usr/local/arduino/libraries Marlin/Marlin.ino
+set -xe
+
+SKETCH_PATH="Marlin/Marlin.ino"
+OUTPUT_BINARY=flash.hex
+BUILD_OPTIONS=/usr/local/arduino/arduino-builder.options
+BUILD_PATH=build
+
+BUILD_PATH=$(realpath "${BUILD_PATH}")
+SKETCH_NAME=$(basename "${SKETCH_PATH}")
+SKETCH_BINARY=${BUILD_PATH}/${SKETCH_NAME}.hex
+
+if [ ! -d ${BUILD_PATH} ]; then
+	mkdir -p ${BUILD_PATH}
+fi
+
+arduino-builder -verbose -fqbn arduino:avr:mega:cpu=atmega2560 -build-path ${BUILD_PATH}  -build-options-file ${BUILD_OPTIONS} -libraries /usr/local/arduino/libraries ${SKETCH_PATH}
+
+mv ${SKETCH_BINARY} ${OUTPUT_BINARY}
