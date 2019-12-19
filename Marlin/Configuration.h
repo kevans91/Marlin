ea31b1e2160012d165f552458d649422b9c9bc87
Kyle Evans <kevans@FreeBSD.org>
2019-12-19 13:44:14 -0600

    Tune a little bit; seems to be under-extruding

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,7 +723,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 332 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.24, 80.24, 1600, 338.78 }
 
 /**
  * Default Max Feed Rate (mm/s)
